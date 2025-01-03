de4e87c4b89f636f8564e63ce17838af8c3006b4
razaiqbal2 <duaf0226@gmail.com>
2025-01-03 16:00:02 +0500

    loop

---
+++ act2.py
@@ -0,0 +1,9 @@
+n=int(input("The number of row  : "))
+
+for i in range( 0 , n+0) :
+
+    for j in range(i):
+
+        print('*',end='')
+
+        print()
---
+++ act3.py
@@ -0,0 +1,2 @@
+f_sum=sum(range(1,11))
+print(f"The total sum of first ten nutural numbers {f_sum}")
